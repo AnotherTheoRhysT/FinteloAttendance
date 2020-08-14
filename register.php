d29b0d1ceb6c20a6a7df9a4508f273ce6168dba4
WINDOWSPC\Administrator <froiacui01@gmail.com>
2020-08-14 11:27:18 +0800

    no message

--- register.php
+++ register.php
@@ -110,6 +110,7 @@
         <div class="container signin">
             <p>Already have an account? <a href="#">Sign in</a>.</p>
         </div>
+        <p>test</p>
     </form>
   </body>
 </html>
