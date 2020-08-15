0163a757956a73a8417d92c175e1fd24d9c6c496
AnotherTheoRhysT <rhyscabonita@gmail.com>
2020-08-15 23:42:08 +0800

    added .gitignore

--- config/config.php
+++ config/config.php
@@ -1,16 +1,26 @@
 <?php
-    $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
 
-    define('ROOT_URL', $root);
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'hPRGOHf6D3p3kWJF');
-    define('DB_NAME', 'fintelo_attendance');
+define('ROOT_URL', $root);
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'hPRGOHf6D3p3kWJF');
+define('DB_NAME', 'fintelo_attendance');
 
-    // Create connection to MySQL DB
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    if(mysqli_connect_errno()){
-        echo 'Failed to connect to MySQL' . mysqli_connect_errno();
-    }
+// Check Connection
+if($mysqli === false) {
+    // IMPROVE ERROR HANDLING
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+
+// // Create connection to MySQL DB
+// $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// if(mysqli_connect_errno()){
+//     echo 'Failed to connect to MySQL' . mysqli_connect_errno();
+// }
+
+// Connect to DB
 ?>
--- register.php
+++ register.php
@@ -7,7 +7,6 @@
     <title>Signin Template for Bootstrap</title>
     
     <!-- Bootstrap core CSS -->
-    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
     * {box-sizing: border-box}
 
@@ -63,20 +62,156 @@
     background-color: #f1f1f1;
     text-align: center;
     }
+
+    .invalid-input{
+        display: block;
+        width: 100%;
+        margin-top: .25rem;
+        font-size: 80%;
+        color: #dc3545;
+    }
     </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head> 
 
-<?php   
+<?php
+
+function console_log($data) {
+    echo '<script>';
+    echo 'console.log('. json_encode( $data ) .')';
+    echo '</script>';
+}
+
 include './config/config.php';
 
-$query = 'SELECT username, password FROM users';
+$email = $username = $psw = $pswConfirm = "";
+$emailErr = $usernameErr = $pswErr = $pswConfirmErr = $serverErr = "";
+$isAdmin = 0;
 
-$result = mysqli_query($conn, $query);
+// IMPROVE SECURITY AND SANITIZATION OF THIS USER INPUT, DAMN IT!!!
 
-// echo '<pre>';
-// print_r($_SERVER);
-// echo '</pre>';
+// Processing of data when POST form is submitted
+if($_SERVER['REQUEST_METHOD'] === "POST") {
+    // Check if new user will be admin
+    $isAdmin = (!empty($_POST['is_admin'])) && ($_POST['is_admin'] === '1') ? 1 : 0 ;
 
+    // Validate email
+    if(empty(trim($_POST['email']))) {
+        //NOTE: IMPROVE EMAIL VALIDATION TO ENFORCE AN EXISTING EMAIL
+        $emailErr = "Please enter email";
+    } else {
+        // Prepare SQL statement
+        $sql = "SELECT id FROM user WHERE email = ?";
+        
+        if($stmt = $mysqli->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("s", $paramEmail);
+
+            // Set parameter
+            $paramEmail = trim($_POST['email']);
+
+            // Attempt to execute prepared statement
+            if($stmt->execute()) {
+                // Store result
+                $stmt->store_result();
+
+                if($stmt->num_rows() === 1) {
+                    $emailErr = "Email already registered. Use another email.";
+                } else {
+                    $email = trim($_POST['email']);
+                }
+            } else { 
+                $serverErr = "Server Error. Please Try Again.";
+            }
+
+            // Close statement;
+            $stmt->close();
+        }
+    }
+
+    // Validate username
+    if(empty(trim($_POST['username']))) {
+        $usernameErr = "Please enter a username.";
+    } else {
+        // Prepare SQL statement
+        $sql = "SELECT id FROM user WHERE username = ?";
+
+        if($stmt = $mysqli->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("s", $paramUsername);
+
+            // Set parameter
+            $paramUsername = trim($_POST['username']);
+
+            // Attempt to exectute the prepared statement
+            if($stmt->execute()) {
+                // Store result
+                $stmt->store_result();
+
+                if($stmt->num_rows == 1) {
+                    $usernameErr = "This username is already taken. Choose another username.";
+                } else {
+                    $username = trim($_POST['username']);
+                }
+            } else {
+                $serverErr = "Server Error. Please Try Again.";
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+    }
+
+    // psw-confirm = $pswConfirm
+    // Validate password
+    if(empty(trim($_POST['psw']))) {
+        $pswErr = "Please enter a password";
+    } elseif(strlen(trim($_POST['psw'])) < 8 ) {
+        $pswErr = "Password must be at least 8 characters";
+    } else {
+        $psw = trim($_POST['psw']);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST['psw-confirm']))) {
+        $pswConfirmErr = "Please reenter password";
+    } else {
+        $pswConfirm = trim($_POST['psw-confirm']);
+        if(empty($pswErr) && ($psw !== $pswConfirm)) {
+            $pswConfirmErr = "Password did not match.";
+        }
+    }
+
+    if(empty($emailErr) && empty($usernameErr) && empty($pswErr) && empty($pswConfirmErr)) {
+        // Prepare insert statement
+        $sql = "INSERT INTO user (`email`, `username`, `password`, `is_admin`) VALUES (?, ?, ?, ?)";
+
+        if($stmt = $mysqli->prepare($sql)) {
+            // Bind variables to the prepared statement as params
+            $stmt->bind_param("sssi", $paramEmailm, $paramUsername, $paramPsw, $paramAdmin);
+            var_dump($stmt->execute());
+            // Set parameters
+            $paramEmail = $email;
+            $paramUsername = $username;
+            $paramPsw = password_hash($psw, PASSWORD_ARGON2ID, ['cost' => 10]);
+            $paramAdmin = $isAdmin;
+
+            // Attempt to execute prepared statement
+            if($stmt->execute()) {
+                // Redirect / Reset to register page
+                header($_SERVER['PHP_SELF']);
+            } else {
+                $serverErr = "Server error. Please try again.";
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+        console_log($isAdmin);
+    }
+    // Close connection
+    $mysqli->close();
+}
 
 ?>
   <body class="text-center">
@@ -86,25 +221,65 @@ $result = mysqli_query($conn, $query);
             <p>Please fill in this form to create an account.</p>
             <hr>
 
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" id="email" required>
+            <div class="form-group">
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="email" id="email" required 
+                    <?php 
+                    echo (!empty($email)) ? "value='{$email}'": "" ;
+                    echo (empty($emailErr)) ? "class='is-valid'" : "class='is-invalid'";
+                    ?> >
+                <?php
+                echo (empty($emailErr)) ? "" : 
+                    "<div class='invalid-input'>{$emailErr}</div>";
+                ?>
+            </div>
+            
+            <div class="form-group">
+                <label for="username"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="username" id="username" required 
+                    <?php 
+                    echo (!empty($username)) ? "value='{$username}'": "" ;
+                    echo (empty($usernameErr)) ? "class='is-valid'" : "class='is-invalid'";
+                    ?> >
+                <?php
+                echo (empty($usernameErr)) ? "" : 
+                    "<div class='invalid-input'>{$usernameErr}</div>";
+                ?>
+            </div>
+            
+            <div class="form-group">
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" id="psw" required 
+                <?php 
+                    echo (!empty($psw)) ? "value='{$psw}'": "" ;
+                    echo (empty($pswErr)) ? "class='is-valid'" : "class='is-invalid'";
+                    ?> >
+                <?php
+                echo (empty($pswErr)) ? "" : 
+                    "<div class='invalid-input'>{$pswErr}</div>";
+                ?>
+            </div>
 
-            <label for="email"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="email" id="email" required>
-
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+            <div class="form-group">
+                <label for="psw-confirm"><b>Repeat Password</b></label>
+                <input type="password" placeholder="Repeat Password" name="psw-confirm" id="psw-repeat" required 
+                <?php 
+                    echo (!empty($pswConfirm)) ? "value='{$pswConfirm}'": "" ;
+                    echo (empty($pswConfirmErr)) ? "class='is-valid'" : "class='is-invalid'";
+                    ?> >
+                <?php
+                echo (empty($pswConfirmErr)) ? "" : 
+                    "<div class='invalid-input'>{$pswConfirmErr}</div>";
+                ?>
+            </div>
 
             <label for="vehicle1">Admin</label><br>
-            <input type="checkbox" id="vehicle1" name="is_admin" value="Bike">
+            <input type="checkbox" name="is_admin" value="1">
             
             <hr>
 
             <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-            <button type="submit" class="registerbtn">Register</button>
+            <input type="submit" class="registerbtn" name="submit" value="Register">
         </div>
 
         <div class="container signin">
